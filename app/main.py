4ba6f8d8a5da22daef8d5ce5b301262ed19c978f
srivarkor <srivardhanakorada@digiframe.org>
2022-01-15 21:46:33 +0530

    Adding main.py in the app

---
+++ app/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get("/all")
+aysnc def all_events():
+    return {
+        "events":{
+            "enagement":{
+                "eventname":"engagement",
+                "fromtime":"210120222330",
+                "venuelat":"28.0235",
+                "venuelong":"52.214"
+            },
+            "reception":{
+                "eventname":"reception",
+                "fromtime":"310120222330",
+                "venuelat":"28.0235",
+                "venuelong":"52.214"
+            },
+            "bachelorparty":{
+                "eventname":"bachelorparty",
+                "fromtime":"260120222230",
+                "venuelat":"30.075",
+                "venuelong":"54.824"
+            }
+        }
+    }
