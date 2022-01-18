33e28c36b32e11d2bdb91a4f8545b7d2dc6fe138
srivarkor <srivardhanakorada@digiframe.org>
2022-01-18 14:56:55 +0530

    Adding CORS

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/events/all")
 async def all_events():
